343ee05214fb037bb7eff749911f53ad125a9bb3
calebgregory <calebgregory@gmail.com>
2015-05-11 15:59:51 -0500

    Adding table to HTML, bootstrap stylesheet, and testing functionality on hiding/showing table

--- main.js
+++ main.js
@@ -9,19 +9,21 @@ function setDisplayValues (data) {
   if (data.Response === "False") { // checks for errors
     var errorMessage = data.Error;
   } else {
+    var poster = data.Poster;
     var title = data.Title;
     var year = data.Year;
     var plot = data.Plot;
+    var rating = data.imdbRating;
 
     $('.search-result').empty();
-    $('.search-result').append(display(title, year, plot, errorMessage));
+    $('.search-result').append(display(poster, title, year, plot, rating, errorMessage));
   }
 }
 
 
 // Takes stored data passed as arguments and creates an document
 // fragment from it, which it then returns.
-function display (title, year, plot, errorMessage) {
+function display (poster, title, year, plot, rating, errorMessage) {
 
   var docFragment = document.createDocumentFragment();
 
@@ -34,6 +36,10 @@ function display (title, year, plot, errorMessage) {
 
   } else { // appends data to docFragment
 
+    var img = document.createElement('IMG');
+    img.setAttribute('src', poster);
+    docFragment.appendChild(img);
+
     var h1 = document.createElement('H1');
     docFragment.appendChild(h1);
     var t = document.createTextNode(title);
@@ -44,17 +50,22 @@ function display (title, year, plot, errorMessage) {
     var y = document.createTextNode(year);
     h2.appendChild(y);
 
-    var p = document.createElement('P');
-    docFragment.appendChild(p);
+    var p_1 = document.createElement('P');
+    docFragment.appendChild(p_1);
+    var ra = document.createTextNode(rating);
+    p_1.appendChild(ra);
+
+    var p_2 = document.createElement('P');
+    docFragment.appendChild(p_2);
     var pl = document.createTextNode(plot);
-    p.appendChild(pl);
+    p_2.appendChild(pl);
 
     var input = document.createElement('INPUT');
     input.setAttribute("type", "button");
     input.setAttribute("name", "add");
     input.setAttribute("value", "+");
     docFragment.appendChild(input);
-    
+
   }
 
   return docFragment;
@@ -72,4 +83,8 @@ $('input[name="search"]').click(function() {
   });
 
   $.get(URL, setDisplayValues);
+
+  $('.saved-result-table').css('visibility','visible');
 })
+
+// On click, adds the data for the current movie
